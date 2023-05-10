b6046f8e7a9dced79bcc6c73eed897136558837e
elias <edwins.elias@ecofrigo.local>
2023-05-10 13:22:30 -0300

    ditado jogo pedra papel o tesoura

--- aula 1/pedra papel tesoura/js/node.js
+++ aula 1/pedra papel tesoura/js/node.js
@@ -1,6 +1,16 @@
 
+
+
+
+
 // numero esculido pelo usuario
-let pergunta = prompt("escolhe, pedra, paple ou tesoura")
+//let pergunta = prompt("escolhe: pedra, paple ou tesoura")
+
+let numeroEscolhido = prompt("escolhe: pedra 1, paple 2 ou tesoura 3")
+
+let jogadaUsuario = parseInt(numeroEscolhido)
+
+
 
 //escolha do computador
 let computador = parseInt(Math.random()*3+1)
@@ -16,25 +26,119 @@ case 2:
     computador = "Tesoura"  
 }
 
+switch(jogadaUsuario){
+    case 1:
+        jogadaUsuario = "pedra" 
+    break;
+    case 2:
+        jogadaUsuario  = "papel"
+      break;
+      case 3:
+        jogadaUsuario = "tesoura" 
+    }
+
+alert(jogadaUsuario)
+
+alert(computador)
+
+
+
+if (jogadaUsuario == computador){
+    alert(`empate computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}else if(jogadaUsuario == "pedra"){
+    if(computador == "papel"){
+        alert(`coputador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+    }else if (computador === "tesoura"){
+         alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+    }}else if(jogadaUsuario === "papel"){
+if(computador == "tesoura"){
+alert(`computador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}else if (computador == "pedra"){
+alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}}else if(jogadaUsuario == "tesoura"){
+
+ if(computador == "pedra"){
+    alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}
+
+else if (computador == "papel"){
+    alert(`computador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}
+
+
+}else if(jogadaUsuario  !== "Pedra"  &&  jogadaUsuario  !==  "Papel" && jogadaUsuario  !== "Tesoura"  ){
+alert(`tristemente ${jogadaUsuario} não esta entre as opções tem que ser um numero do 1 ao 3`) 
+}if (jogadaUsuario == computador){
+        alert(`empate computador escolhor ${computador} e voce ${jogadaUsuario}`)
+    } else if(jogadaUsuario == "pedra"){
+        if(computador == "papel"){
+            alert(`coputador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+        }else if (computador === "tesoura"){
+             alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+        }}
+
+
+
+
+    
+        else if(jogadaUsuario === "papel"){
+ if(computador == "tesoura"){
+    alert(`computador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}
+
+else if (computador == "pedra"){
+    alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+}}
+
+// se o jogador escolher tesoura 
+
+
+else if(jogadaUsuario == "tesoura"){
+
+     if(computador == "pedra"){
+        alert(`voce ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+    }
+
+    else if (computador == "papel"){
+        alert(`computador ganho por que computador escolhor ${computador} e voce ${jogadaUsuario}`)
+    }
+
+
+}
+ //se for diferente dos numeros
+ else if(jogadaUsuario  !== "Pedra"  &&  jogadaUsuario  !==  "Papel" && jogadaUsuario  !== "Tesoura"  ){
+alert(`tristemente ${jogadaUsuario} não esta entre as opções tem que ser um numero do 1 ao 3`) 
+}
 
 
 
 
 
 
-if (pergunta === computador){
-    let aviso = alert(`empate computador escolhor ${computador} e voce ${pergunta}`)
+        
+    
+
+
+
+
+
+
+
+
+
+/*if (pergunta === computador){
+    alert(`empate computador escolhor ${computador} e voce ${pergunta}`)
 }else if(pergunta==="pedra"){
     if(computador ==="papel"){
-        let aviso = alert(`coputador ganho por que computador escolhor ${computador} e voce ${pergunta}`)
+        alert(`coputador ganho por que computador escolhor ${computador} e voce ${pergunta}`)
     }else if(computador ==="tesoura"){
-        let aviso = alert(`voce ganho por que computador escolhor ${computador} e voce ${pergunta}`)
+         alert(`voce ganho por que computador escolhor ${computador} e voce ${pergunta}`)
     }}
     else if(pergunta==="papel"){
         if(computador ==="pedra"){
-            let aviso = alert(`voce ganho por que computador escolhor ${computador} e voce ${pergunta}`)
+            alert(`voce ganho por que computador escolhor ${computador} e voce ${pergunta}`)
         }else if(computador ==="tesoura"){
-            let aviso = alert(`computador ganho por que computador escolhor ${computador} e voce ${pergunta}`)
+             alert(`computador ganho por que computador escolhor ${computador} e voce ${pergunta}`)
         }
-}
+}*/
 
