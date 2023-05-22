c2407f81a21ba8354ecc4b48bcd94907d27aea61
elias <25elias.c@gmail.com>
2023-05-22 17:22:02 -0300

    editando aula 5

--- aula 5/Aula5_mesa_de_trabalho/Aula5_mesa_de_trabalho/js/main.js
+++ aula 5/Aula5_mesa_de_trabalho/Aula5_mesa_de_trabalho/js/main.js
@@ -4,12 +4,13 @@ const body_elemenRef = document.querySelector(".elemen")
 
 const header_mainRef = document.querySelector('.header_main')
 
+const itemRef = document.querySelectorAll(".item")
 
-const itemRef = document.querySelector(".item")
 
 
-const text_itensRfe = document.querySelector(".text_itens")
+const text_itensRfe = document.querySelectorAll(".text_itens")
 
+const pReferen = document.querySelectorAll("p")
 
 const button = document.querySelector("button")
 
@@ -20,8 +21,36 @@ function DarkMode(){
     
         body_elemenRef.classList.toggle("dark")
         header_mainRef.classList.toggle("header_dark")
-        itemRef.classList.toggle("item_dark")
-        text_itensRfe.classList.toggle("text_itens_dark")
+        
+        
+        
+        
+        for(let i = 0; i < itemRef.length; i++){
+               itemRef[i].classList.toggle("item_dark")}
+
+
+         for(let b = 0; b < text_itensRfe.length; b++){
+
+          text_itensRfe[b].classList.toggle("text_itens_dark")
+
+
+         }    
+         
+         
+
+         for(let c=0;c<pReferen.length;c++){
+
+            
+               pReferen[c].classList.toggle('text_dark')
+
+
+
+
+         }
+
+        
+
+
         button.innerText="Mudar para Escuro"
 
 if(body_elemenRef.classList.contains("dark")  ){
