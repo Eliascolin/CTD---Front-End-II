635a318514233b2622b2b3d59c939f367c04250e
Eliascolin <eliascolina.c@gmail.com>
2023-05-29 22:37:42 -0300

    inicio do Primeiro Check Point

---
+++ Primeiro Check Point/main/main.js
@@ -0,0 +1,5 @@
+//https://docs.google.com/document/d/1xoosin6AZL3p8XJlMEge7_iyAmx6LTsg7vpop-j7UHY/edit
+
+//https://www.figma.com/file/XMNFWdQDJOsHjogi2t4dhu/%5BReDesign%5D-Check-Point-1-(Front-end-2)?type=design&node-id=901-87&t=cIhnaY3aP4KlfBEb-0
+
+//https://www.google.com/search?q=avatar+de+pessoas&sxsrf=APwXEddrKKnvKV4vQJTmh8FPvB1zmtJyJQ:1685403091765&source=lnms&tbm=isch&sa=X&ved=2ahUKEwia4Nnk15v_AhUTBLkGHYyeC3YQ_AUoAXoECAEQAw&biw=1366&bih=617&dpr=1#imgrc=I0Kzpk2VHNzArM
---
+++ aula 7/main/main.js
@@ -0,0 +1,36 @@
+const inputRef = document.querySelector('.input')
+
+const buttonRef= document.querySelector('.button')
+
+const divRef = document.querySelector(".resultado")
+
+
+let nomeUsuario = ''
+
+
+function exibir(){
+
+console.log("nome")
+divRef.innerHTML = nomeUsuario
+}
+
+function cadastro(event){
+
+    nomeUsuario = event.target.value
+
+}
+
+
+
+buttonRef.addEventListener('click', () =>exibir())
+
+
+inputRef.addEventListener('keyup',(event)=>cadastro(event)
+
+
+
+
+
+)
+
+
