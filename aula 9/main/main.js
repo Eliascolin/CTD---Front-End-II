4eb8c3cd9506a9475ec2d413d707360c14c3c3ed
elias <25elias.c@gmail.com>
2023-06-02 16:43:16 -0300

    b

--- aula 9/main/main.js
+++ aula 9/main/main.js
@@ -2,30 +2,100 @@ const userNameInputRef = document.querySelector("#userNameInput")
 
 const logingButtonRef = document.querySelector("#logingButton")
 
+const formControlNomeRef = document.querySelector("#formControlNome")
+
+const formControlPasswordRef = document.querySelector("#formControlPassword")
 
 
-function validateUserName(event){
+var user =  {
+
+name:"",
+password:""
+
+
+
+}
+
+
+
+
+function validateUserPassword(event){
 const target = event.target
 const value = target.value
 const isValid = target.checkValidity()
 
 
 
-console.log(value)
+user.password = value
 
 if(isValid){
+
+    formControlPasswordRef.classList.remove("error")
+
 console.log("Valor invalido")
 
 
 }else{
-    console.log("valor valido")}
+    console.log("valor valido")
+
+    formControlPasswordRef.classList.add("error")
+
+
+}
 
 }
 
 
+function validateUserName(event){
+    const target = event.target
+    const value = target.value
+    const isValid = target.checkValidity()
+    
+    
+    
+    user.name = value
+    
+    if(isValid){
+    
+    formControlNomeRef.classList.remove("error")
+    
+    
+
+    
+    
+    }else{
+        console.log("valor valido")
+    
+        formControlNomeRef.classList.add("error")
+    
+    
+    }}
+    
+    
+
+    function login(event){
+
+event.preventDefault
+
+console.log(user)
+
+
+    }
+
+
+
+
+
 userNameInputRef.addEventListener('keyup', (event)=>validateUserName(event))
-                                                       
+          
+
+//configurar senha 
+
+formControlPasswordRef.addEventListener("keyup",(event)=>validateUserPassword(event))
 
 
                                                           //evita que se comporte default
-logingButtonRef.addEventListener("click",(event) => {event.preventDefault()})
+logingButtonRef.addEventListener("click",(event) => login(event))
+
+
+
